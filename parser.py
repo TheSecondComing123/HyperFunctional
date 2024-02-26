ec506217e573910e5c4a66ea5ef13e5f26513aa0
Alan Bagel <jiuprogrammer@gmail.com>
2024-02-26 16:41:00 -0500

    added faulty string support

--- parser.py
+++ parser.py
@@ -5,32 +5,36 @@ from lexer import HFLexer
 class HFParser(sly.Parser):
 	tokens = HFLexer.tokens
 	
-	@_('expr SEP expr PLUS')
+	@_("expr SEP expr PLUS")
 	def expr(self, p):
 		return p[0] + p[2]
 	
-	@_('expr SEP expr MINUS')
+	@_("expr SEP expr MINUS")
 	def expr(self, p):
 		return p[0] - p[2]
 	
-	@_('expr SEP expr TIMES')
+	@_("expr SEP expr TIMES")
 	def expr(self, p):
 		return p[0] * p[2]
 	
-	@_('expr SEP expr EXPO')
+	@_("expr SEP expr EXPO")
 	def expr(self, p):
 		return p[0] ** p[2]
 	
-	@_('expr SEP expr DIVIDE')
+	@_("expr SEP expr DIVIDE")
 	def expr(self, p):
 		val = p[0] / p[2]
 		if val == int(val):
 			return int(val)
 		return round(val, 2)
 	
-	@_('NUMBER')
+	@_("NUMBER")
 	def expr(self, p):
 		return p[0]
+	
+	@_("STRING")
+	def expr(self, p):
+		return p.STRING
 
 
 parser = HFParser()
