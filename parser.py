d4d1a5c5bef7069881c170d23b4022fad67f98c5
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2024-02-27 08:48:21 -0500

    added string division

--- parser.py
+++ parser.py
@@ -25,10 +25,11 @@ class HFParser(sly.Parser):
 	
 	@_("expr SEP expr DIVIDE")
 	def expr(self, p):
-		val = p[0] / p[2]
-		if val == int(val):
-			return int(val)
-		return round(val, 2)
+		# val = p[0] / p[2]
+		# if val == int(val):
+		# 	return int(val)
+		# return round(val, 2)
+		return divide(p[0], p[2])
 	
 	@_("NUMBER")
 	def expr(self, p):
