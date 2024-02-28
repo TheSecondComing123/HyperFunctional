7c91feeb8bee123133c41d443da7f8a0f4b7ac21
Alan Bagel <jiuprogrammer@gmail.com>
2024-02-28 13:14:34 -0500

    added lists yeay

--- parser.py
+++ parser.py
@@ -37,6 +37,27 @@ class HFParser(sly.Parser):
 	def expr(self, p):
 		return p.STRING
 	
+	@_("LIST")
+	def expr(self, p):
+		return p.LIST
+	
+	@_("LIST_BEGIN list_items LIST_END")
+	def LIST(self, p):
+		return p.list_items
+	
+	@_("list_items LIST_SEP item")
+	def list_items(self, p):
+		p.list_items.append(p.item)
+		return p.list_items
+	
+	@_("item")
+	def list_items(self, p):
+		return [p.item]
+	
+	@_("expr")
+	def item(self, p):
+		return p[0]
+	
 	@_("LOWERCASE_ALPHABET")
 	def expr(self, p):
 		return ascii_lowercase
